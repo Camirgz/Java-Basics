17358340d47f878de34dc07eda51373177f32267
Camila Rodriguez <camila.rodriguez.aguila@gmail.com>
2022-01-25 15:22:28 -0700

    Displays a message instructing to enter a text and then prints it

--- MyClass.java
+++ MyClass.java
@@ -1,4 +1,12 @@
-public class MyClass {
+import java.util.Scanner;
+
+
+class MyClass {
+
+
     public static void main(String[ ] args) {
-        System.out.println("Hello World! My name is Camila!");
-}}
+            System.out.println("Input a text: ");
+            Scanner myVar = new Scanner(System.in);
+            System.out.println(myVar.nextLine());
+        }
+    }
