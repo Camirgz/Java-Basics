d7e84708fa8aa3d44e3995c62175c81ab0d2c3f3
Camila <camila.rodriguez.aguila@gmail.com>
2022-03-19 07:33:51 -0700

    Commented code and concatenated the result with the same number 1 and number 2 variables instead of creating a result one

--- Math_Operators.java
+++ Math_Operators.java
@@ -1,14 +1,22 @@
+// import the package for taking user input
 import java.util.Scanner;
 
 public class Math_Operators {
         public static void main(String[ ] args) {
+
+//          Variable initialization
+            int number1;
+            int number2;
+
+//          Scanner Object Creation, Name = input
             Scanner input = new Scanner(System.in);
+
+//          User Interaction with scanner object usage for receiving input
             System.out.printf("Enter your first number: ");
-            int number1 = input.nextInt();
+            number1 = input.nextInt();
             System.out.printf("Enter your second number: ");
-            int number2 = input.nextInt();
-            int result = number1+number2;
-            System.out.println(number1 + " + " + number2 + " = " + result);
+            number2 = input.nextInt();
+//          Print the result of the numbers
+            System.out.println(number1 + " + " + number2 + " = " + (number1+number2));
         }
 }
-
