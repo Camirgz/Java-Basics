b5beb093d726ec71cc7ceaa22c7d524a1d707b4e
Camila <camila.rodriguez.aguila@gmail.com>
2022-03-19 07:50:04 -0700

    if decision inclusion

--- Math_Operators.java
+++ Math_Operators.java
@@ -7,16 +7,35 @@ public class Math_Operators {
 //          Variable initialization
             int number1;
             int number2;
+            String decision;
 
 //          Scanner Object Creation, Name = input
             Scanner input = new Scanner(System.in);
 
 //          User Interaction with scanner object usage for receiving input
-            System.out.printf("Enter your first number: ");
+            System.out.print("Enter your first number: ");
             number1 = input.nextInt();
-            System.out.printf("Enter your second number: ");
+            System.out.print("Enter your second number: ");
             number2 = input.nextInt();
-//          Print the result of the numbers
-            System.out.println(number1 + " + " + number2 + " = " + (number1+number2));
+            System.out.print("Enter the operation you would like to commit to those numbers: ");
+            decision = input.next();
+
+//            If Decision and result printing
+            if (decision.equals("+"))
+            {
+                System.out.println(number1 + " + " + number2 + " = " + (number1+number2));
+            }
+            if (decision.equals("-"))
+            {
+                System.out.println(number1 + " - " + number2 + " = " + (number1-number2));
+            }
+            if (decision.equals("/"))
+            {
+                System.out.println(number1 + " / " + number2 + " = " + (number1/number2));
+            }
+            if (decision.equals("*"))
+            {
+                System.out.println(number1 + " * " + number2 + " = " + (number1*number2));
+            }
         }
 }
