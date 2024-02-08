1a776a0d5881e463fbef1766b59e153057f25da7
Camila <camila.rodriguez.aguila@gmail.com>
2024-02-07 20:47:22 -0700

    Hasta el minuto 30

--- Práctica1/PilaDinamica.java
+++ Práctica1/PilaDinamica.java
@@ -1,4 +1,62 @@
 package Práctica1;
 
-public class PilaDinamica {
+public class PilaDinamica<T> {
+
+    private Nodo<T> top;
+    private int tamanio;
+
+    public PilaDinamica(){
+        top = null;
+        this.tamanio = 0;
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public int size(){
+        return this.tamanio;
+    }
+
+    public T top(){
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            return top.getElemento();
+        }
+    }
+
+    public T pop(){
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            T elemento = top.getElemento();
+            Nodo<T> aux = top.getSiguiente();
+            top = null;
+            top = aux;
+            this.tamanio--;
+            return elemento;
+        }
+    }
+
+    public void push(T elemento){
+        Nodo<T> aux = new Nodo<T>(elemento, top);
+        top = aux;
+        this.tamanio++;
+
+    }
+
+    public String toString(){
+        if(isEmpty()){
+            return "La pila esta vacia";
+        }
+        else{
+            Nodo<T> aux = top;
+            while(aux != null){
+                aux = aux.getSiguiente();
+            }
+        }
+    }
 }
